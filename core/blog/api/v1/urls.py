72fdbee3b7ed713dd786e19f699c676cc7cd8bbd
mrrahbarnia <mrrahbarnia@gmail.com>
2023-08-30 13:00:37 +0330

    Generation APIView
    Migrating to APIView from api function based view

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "api-v1"
 
 urlpatterns = [
-    path('post/',views.post_list_view,name = "post-list"),
-    path('post/<int:id>/',views.post_detail_view,name = "post-detail"),
+    # path('post/',views.post_list_view,name = "post-list"),
+    # path('post/<int:id>/',views.post_detail_view,name = "post-detail"),
+    path('post/',views.PostListApiView.as_view(),name= "post-list"),
+    path('post/<int:id>',views.PostSingleApiView.as_view(),name='post-single'),
+    
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,12 +1,13 @@
-from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from ...models import Post
 from .serializers import PostSerialazer
-from rest_framework import status
+from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+# from rest_framework.decorators import api_view,permission_classes
 
-
+"""
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def post_list_view(request):
@@ -19,8 +20,25 @@ def post_list_view(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+"""
 
+class PostListApiView(APIView):
+    """Getting a list of posts and creating new posts"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerialazer
+    def get(self,request):
+        """Retrieving a list of posts"""
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerialazer(posts,many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        """Creating a post with provided data"""
+        serializer = PostSerialazer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
+"""
 @api_view(['GET','PUT','DELETE'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def post_detail_view(request,id):
@@ -42,3 +60,28 @@ def post_detail_view(request,id):
     elif request.method == 'DELETE':
         post.delete()
         return Response({"detail":"item deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+"""
+
+class PostSingleApiView(APIView):
+    """Getting a single post by it's id plus eitting and removing it"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerialazer
+    def get(self,request,id):
+        """Retrieving the detail of a post by it's id"""
+        post = get_object_or_404(Post,pk=id)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+    def put(self,request,id):
+        """Editting a post by it's id"""
+        post = get_object_or_404(Post,pk=id)
+        serializer = self.serializer_class(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    def delete(self,request,id):
+        """Deleting a post by it's id"""
+        post = get_object_or_404(Post,pk=id)
+        post.delete()
+        return Response({"detail":"item deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+
+        
