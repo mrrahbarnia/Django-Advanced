e58a0b79a1925f770d1d4f154ff2adb79112d5d1
mrrahbarnia <mrrahbarnia@gmail.com>
2023-09-22 12:14:14 +0330

    Refactoring by flake8 for CI testing

--- core/blog/forms.py
+++ core/blog/forms.py
@@ -5,4 +5,4 @@ from blog.models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content", "status","category","published_date"]
+        fields = ["title", "content", "status", "category", "published_date"]
