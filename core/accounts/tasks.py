32935673250190980e1eb25cc3b33ed60b8f0a86
mrrahbarnia <mrrahbarnia@gmail.com>
2023-09-28 17:13:03 +0330

    refactoring flake8 for CI

--- core/accounts/tasks.py
+++ core/accounts/tasks.py
@@ -2,7 +2,8 @@ from celery import shared_task
 
 from time import sleep
 
+
 @shared_task
 def send_email():
     sleep(3)
-    print ("email sent")
+    print("email sent")
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -7,11 +7,15 @@ import requests
 
 from .tasks import send_email
 
+
 def email(request):
     send_email.delay()
     return HttpResponse("<h1>Done</h1>")
 
+
 @cache_page(60)
 def caching(request):
-    r = requests.get("https://30ad6a60-9542-46ac-a2c0-1005df1c504a.mock.pstmn.io/test/5")
+    r = requests.get(
+        "https://30ad6a60-9542-46ac-a2c0-1005df1c504a.mock.pstmn.io/test/5"
+        )
     return JsonResponse(r.json())
